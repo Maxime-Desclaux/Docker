4eea123819ba9c72ed96868099f77f22476f9338
Maxime Desclaux <maxime@desclaux.fr>
2025-12-18 11:57:22 +0100

    ajout du microservice

--- php_service/index.php
+++ php_service/index.php
@@ -1,13 +1,3 @@
 <?php
-header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');
-
-$prenom = getenv('PRENOM') ?: 'Maxime';
-
-echo json_encode([
-    'prenom' => $prenom,
-    'service' => 'PHP Microservice',
-    'timestamp' => date('Y-m-d H:i:s'),
-    'status' => 'OK'
-]);
+echo "Maxime";
 ?>
